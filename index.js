3ca84b81da9e857d92db3252b882feef5422591b
Sunny Kumar <goodluck14115@gmail.com>
2024-07-08 17:05:01 +0530

    Starting point

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+const app = require('./app')
+const { info } = require('./utils/logger')
+
+const PORT = 3000
+app.listen(PORT, () => {
+  info(`Server is running on port ${PORT}`)
+})
