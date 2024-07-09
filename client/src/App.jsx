cc8f95cc5eec1c2267a1ba73edf5d2b2e3566d5c
Sunny Kumar <goodluck14115@gmail.com>
2024-07-09 10:12:22 +0530

    Install vite react

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-orange-400">Hello world</h1>
+    </div>
+  )
+}
