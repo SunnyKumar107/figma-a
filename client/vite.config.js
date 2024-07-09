6c4aa8832d7f8ff752af17244f24da3d72f6aa46
Sunny Kumar <goodluck14115@gmail.com>
2024-07-09 22:29:02 +0530

    Update baseUrl

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': { target: 'http://localhost:3000', changeOrigin: true }
+      '/api': { target: 'https://figma-a.onrender.com', changeOrigin: true }
     }
   }
 })
