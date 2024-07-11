bc743da8784df0c87a0fc821af6c0a403388ff43
Sunny Kumar <goodluck14115@gmail.com>
2024-07-11 17:30:40 +0530

    Create home page

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,15 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { userContext } from '../context/context'
 import { useNavigate } from 'react-router-dom'
+import { FaCheck } from 'react-icons/fa'
 
 const Home = () => {
   const { user, setUser } = useContext(userContext)
   const navigate = useNavigate()
 
-  if (!user) {
-    navigate('/sign-in')
-    return
-  }
+  useEffect(() => {
+    if (!user) {
+      navigate('/sign-in')
+    }
+  }, [])
 
   const handleLogout = async () => {
     setUser(null)
@@ -19,17 +21,35 @@ const Home = () => {
   }
 
   return (
-    <div className="flex flex-col gap-1 items-center">
-      <div>
-        <h1 className="text-4xl font-medium">{user.name}</h1>
-        <p className="text-sm text-gray-500 mt-2">{user.email}</p>
+    <div
+      className="relative min-h-screen bg-cover bg-center bg-no-repeat flex flex-col gap-1 items-center"
+      style={{
+        backgroundImage:
+          'url(https://s3-alpha-sig.figma.com/img/e088/8995/13a478aa6d3cc9bebac1c6fe29b1cf35?Expires=1721606400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=duz6dA7doe3wpqGBTvFXD7eSqCD1lBv39rMRvpbxVxWU-HwqrRREYNRE9el2I7LQ0kPjAVZa9CdyOW-EhrkuDHLn0Qw-X5F~mxs7hKcbyrgiXQbv6IbgDiAy8Eze4ErKeBRmZhXOjIUdhSN5SRgZaip8TlZe3--BCk-4yKi2c88lC~V73tXAVaV5xowjy6WYbeO5g-NSXxbY9PGnPx-t1pcidgKfTY-DojLcSVzrByxyw7gfGL5tdI0BsKbc12Ynu8MW4rnc-Ctmc0S3TE7ICdWL22FaTsKhQT0h7N6Nkd5SF6y8M7Lmp8fwbnH8AAPVWa0AtlqcLFrnTJg8ExaFVw__)'
+      }}
+    >
+      <div className="w-full flex flex-col items-center sm:max-w-[600px] h-[492px] rounded-t-3xl absolute bottom-0 bg-[#ffffff] px-6 pb-6">
+        <div className="flex justify-center p-2">
+          <div className="w-[58.13px] h-[4px] bg-[#878787] rounded-[11px]"></div>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-8 p-10 mt-6">
+          <div className="w-24 h-24 flex items-center justify-center rounded-full border-dotted border-8 border-[#FE8C00] text-[#FE8C00]">
+            <FaCheck size={35} />
+          </div>
+          <h1 className="text-[#101010] font-semibold text-2xl">
+            Login Successful
+          </h1>
+        </div>
+        <button className="bg-[#FE8C00] w-full text-white rounded-[100px] p-4 text-sm font-semibold hover:bg-[#FE8C00]/80 h-[52px]">
+          Go to Tracking Screen
+        </button>
+        <button
+          className="text-sm font-semibold text-[#878787] hover:text-[#878787]/80 mt-4"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
       </div>
-      <button
-        className="mt-4 bg-slate-900 text-white rounded-lg p-2"
-        onClick={handleLogout}
-      >
-        Log out
-      </button>
     </div>
   )
 }
